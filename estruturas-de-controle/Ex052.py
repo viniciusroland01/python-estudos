cc59622b40a8b0236afdd6e3cd12187cd86e6e90
viniciusroland01 <viniciusroland01@hotmail.com>
2026-03-11 23:17:29 -0300

    Adicionando novos exercícios de python

--- estruturas-de-controle/Ex052.py
+++ estruturas-de-controle/Ex052.py
@@ -19,10 +19,10 @@ while True:
     t=0
     for c in range (1, n + 1):
         if n % c == 0 :
-            print('\033[33m' , end='')
+            print('\033[31m' , end='')
             t += 1
         else:
-            print('\033[31m' , end='')
+            print('\033[m' , end='')
         print(f'{c} ' , end= '')
     print('\033[m')
     if t == 2:
